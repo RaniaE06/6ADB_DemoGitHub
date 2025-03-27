6e291e6aa12b49cb989a9c596e0666fd2beb5515
RaniaE06 <rania.uiltje@gmail.com>
2025-03-27 11:33:00 +0100

    Add project files.

---
+++ 6ADB_DemoGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6ADB_DemoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mijn eerste solution die beschikbaar is via GitHub");
+        }
+    }
+}
