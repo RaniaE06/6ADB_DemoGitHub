c67e42faf67f26af8d301ca698716e1c8debfc4d
RaniaE06 <rania.uiltje@gmail.com>
2025-03-27 11:44:21 +0100

    eerste update

--- 6ADB_DemoGitHub/Program.cs
+++ 6ADB_DemoGitHub/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Mijn eerste solution die beschikbaar is via GitHub");
+            Console.WriteLine("Dit is een update van de vorige versie");
         }
     }
 }
